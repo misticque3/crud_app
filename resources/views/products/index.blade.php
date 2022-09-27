5635128bd0cb3f78c775b08cdbe9eb091c6d0316
Milos Zaric <misticque3@yahoo.com>
2022-09-27 13:44:56 +0200

    header change

--- resources/views/products/index.blade.php
+++ resources/views/products/index.blade.php
@@ -4,7 +4,7 @@
     <div class="row">
         <div class="col-lg-12 margin-tb">
             <div class="pull-left">
-                <h2>Laravel 9 CRUD Example from scratch - ItSolutionStuff.com</h2>
+                <h2>Laravel 9 CRUD Example from scratch - ItSolutionStuff.com test</h2>
             </div>
             <div class="pull-right">
                 <a class="btn btn-success" href="{{ route('products.create') }}"> Create New Product</a>
